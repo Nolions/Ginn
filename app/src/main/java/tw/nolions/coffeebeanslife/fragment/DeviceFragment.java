f312bf156cbd5324500624548bfe58acb6055c11
KevinChen <lv2410@gmail.com>
2019-03-13 01:01:51 +0800

    [revise] 更新DeviceList's toolbar

--- app/src/main/java/tw/nolions/coffeebeanslife/fragment/DeviceFragment.java
+++ app/src/main/java/tw/nolions/coffeebeanslife/fragment/DeviceFragment.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -30,12 +31,16 @@ public class DeviceFragment extends Fragment{
     private Toolbar mToolBar;
     private ListView mDeviceListView;
 
+    private Boolean mScanning = true;
     private String TAG;
     private BluetoothDeviceAdapter mDeviceListAdapter;
     private BluetoothAdapter mBluetoothAdapter;
     private Set<BluetoothDevice> mPairedDevices;
 
     private final int REQUEST_ENABLE_BT = 1;
+    private static final long SCAN_PERIOD = 10000;
+
+    private Handler mHandler;
 
 
     @Override
@@ -43,6 +48,7 @@ public class DeviceFragment extends Fragment{
         super.onCreate(savedInstanceState);
 
         TAG = getResources().getString(R.string.app_name);
+        mHandler = new Handler();
     }
 
     @Override
@@ -62,13 +68,26 @@ public class DeviceFragment extends Fragment{
     @Override
     public void onResume() {
         super.onResume();
-        getPairedDevices();
+        scanDevice(true);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        Log.d(TAG, "onCreateOptionsMenu");
         menu.clear();
         inflater.inflate(R.menu.menu_device_list, menu);
+
+        if (!mScanning) {
+            menu.findItem(R.id.menu_stop).setVisible(false);
+            menu.findItem(R.id.menu_scan).setVisible(true);
+            menu.findItem(R.id.menu_refresh).setActionView(null);
+        } else {
+            menu.findItem(R.id.menu_stop).setVisible(true);
+            menu.findItem(R.id.menu_scan).setVisible(false);
+            menu.findItem(R.id.menu_refresh).setActionView(R.layout.actionbar_indeterminate_progress);
+        }
+
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
@@ -79,8 +98,15 @@ public class DeviceFragment extends Fragment{
                 Log.d(TAG, "Back pressure fragment");
                 getFragmentManager().popBackStack();
                 return true;
-            case R.id.bluetooth_search:
-                Log.d(TAG, "on click toolbar's bluetooth search button...");
+            case R.id.menu_scan:
+                Log.d(TAG, "scan");
+                scanDevice(true);
+//                invalidateOptionsMenu();
+                break;
+            case R.id.menu_stop:
+                Log.d(TAG, "stop");
+                scanDevice(false);
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -135,5 +161,23 @@ public class DeviceFragment extends Fragment{
         }
 
         this.changeDeviceList();
+    }
+
+    private void scanDevice(final boolean enable) {
+
+        if (enable) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    getPairedDevices();
+                    getActivity().invalidateOptionsMenu();
+                }
+            }, SCAN_PERIOD);
+            mScanning = true;
+        } else {
+            mScanning = false;
+        }
+        getActivity().invalidateOptionsMenu();
     }
 }
