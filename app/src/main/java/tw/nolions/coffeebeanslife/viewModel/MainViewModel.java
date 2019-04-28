59ec206d5b70cdddbb1101d42efa3cbdcb4165db
KevinChen <lv2410@gmail.com>
2019-04-28 19:00:14 +0800

    [change] 溫度設定改用seekbar表示

--- app/src/main/java/tw/nolions/coffeebeanslife/viewModel/MainViewModel.java
+++ app/src/main/java/tw/nolions/coffeebeanslife/viewModel/MainViewModel.java
@@ -1,12 +1,14 @@
 package tw.nolions.coffeebeanslife.viewModel;
 
+import android.app.Activity;
 import android.arch.lifecycle.ViewModel;
-import android.content.Context;
 import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -15,22 +17,32 @@ import java.util.HashMap;
 import tools.Convert;
 import tools.info;
 import tw.nolions.coffeebeanslife.R;
+import tw.nolions.coffeebeanslife.callback.ViewModelCallback;
 
 public class MainViewModel extends ViewModel {
+//    private
     private final int TEMP_RANGE = 5;
     private String relayStatus = "c";
 
     public final ObservableField<String> mBeansTemp, mStoveTemp, mEnvironmentTemp, mTargetTemp;
     public final ObservableBoolean isImport;
 
-    private Context mContext;
+    private ViewModelCallback mViewModelCallback;
+
+    private Activity mActivity;
     private Handler mHandler;
     private OutputStream mOutputStream;
 
     private int mNowSetTemp = 0;
 
-    public MainViewModel(Context context) {
-        mContext = context;
+    public MainViewModel(Fragment fragment) {
+        mActivity = fragment.getActivity();
+
+        try {
+            mViewModelCallback = (ViewModelCallback) fragment;
+        } catch (Exception e) {
+            Log.e(info.TAG(), e.getMessage());
+        }
 
         mBeansTemp = new ObservableField<>();
         mStoveTemp = new ObservableField<>();
@@ -60,17 +72,17 @@ public class MainViewModel extends ViewModel {
                     if (data.equals("c") || data.equals("o")) {
                         // 設定繼電器狀態
                         if (isImport.get() == true) {
-                            mOutputStream.write("o".getBytes());
+                            mOutputStream.write("c\r".getBytes());
                             isImport.set(false);
                             setTargetTemp("0");
                         } else {
-                            mOutputStream.write("c".getBytes());
+                            mOutputStream.write("o\r".getBytes());
                             isImport.set(true);
                         }
                     } else if(data.equals("add")) {
                         // 設定目標溫度
                         addTemp();
-                        String temp = ""+ getNowSetTemp();
+                        String temp = ""+ getNowSetTemp()+"\r";
                         mOutputStream.write(temp.getBytes());
                         setTargetTemp("" + getNowSetTemp());
                     } else if(data.equals("less")) {
@@ -104,51 +116,44 @@ public class MainViewModel extends ViewModel {
     }
 
     public void setBeansTemp(String temp) {
-        this.mBeansTemp.set(temp +  mContext.getResources().getString(R.string.tempUnit));
+        this.mBeansTemp.set(temp +  mActivity.getResources().getString(R.string.tempUnit));
     }
 
     public void setStoveTemp(String temp) {
-        this.mStoveTemp.set(temp +  mContext.getResources().getString(R.string.tempUnit));
+        this.mStoveTemp.set(temp +  mActivity.getResources().getString(R.string.tempUnit));
     }
 
     public void setEnvironmentTemp(String temp) {
-        this.mEnvironmentTemp.set(temp +  mContext.getResources().getString(R.string.tempUnit));
+        this.mEnvironmentTemp.set(temp +  mActivity.getResources().getString(R.string.tempUnit));
     }
 
-    public void onClickTempPlusActionButton() {
-        Log.d(info.TAG(), "MainViewModel::onClickTempPlusActionButton()");
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Message msg = Message.obtain();
-                msg.obj = "add";
+    /**
+     * 變更目標溫度
+     * @param seekBar
+     * @param progresValue
+     * @param fromUser
+     */
+    public void onTargetTempChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+        Log.d(info.TAG(), "MainViewModel::onTargetTempChanged(), progress:"+ progresValue );
+        this.setTargetTemp(String.valueOf(progresValue));
 
-                mHandler.sendMessage(msg);
-            }
-        });
+//        mViewModelCallback.updateTargetTemp("10");
+//        Thread t = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Message msg = Message.obtain();
+//                msg.obj = "add";
+//
+//                mHandler.sendMessage(msg);
+//            }
+//        });
 
-        t.start();
-    }
-
-    public void onClickTempLessActionButton() {
-        Log.d(info.TAG(), "MainViewModel::onClickTempLessActionButton()");
-
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Message msg = Message.obtain();
-                msg.obj = "less";
-
-                mHandler.sendMessage(msg);
-            }
-        });
-
-        t.start();
+//        t.start();
     }
 
     public void onClickBeanImportActionButton() {
         Log.d(info.TAG(), "MainViewModel::onClickBeanImportActionButton()");
-
+        mViewModelCallback.startAction(true);
         if (isImport.get() == true) {
             relayStatus = "c";
         } else {
@@ -168,12 +173,54 @@ public class MainViewModel extends ViewModel {
         t.start();
     }
 
-    public void onClickOneBoom() {
-        Log.d(info.TAG(), "MainViewModel::onClickOneBoom()");
+    /**
+     * 下豆按鈕
+     * @return
+     */
+    public boolean startBeansClick() {
+        Log.d(info.TAG(), "MainViewModel::startBeansClick()");
+        mViewModelCallback.startAction(true);
+        return true;
     }
 
-    public void onClickTwoBoom() {
-        Log.d(info.TAG(), "MainViewModel::onClickTwoBoom()");
+    /**
+     * 出豆按鈕
+     * @return
+     */
+    public boolean stopBeansClick() {
+        Log.d(info.TAG(), "MainViewModel::stopBeansClick()");
+        mViewModelCallback.startAction(false);
+        return true;
+    }
+
+    /**
+     * 一爆按鈕
+     */
+    public void onFirstCrack() {
+        mViewModelCallback.firstCrack();
+        Log.d(info.TAG(), "MainViewModel::onFirstCrack()");
+    }
+
+    /**
+     * 二爆按鈕
+     */
+    public void onSecondCrack() {
+        mViewModelCallback.secondCrack();
+        Log.d(info.TAG(), "MainViewModel::onSecondCrack()");
+    }
+
+    /**
+     * 更新View Model上溫度資訊
+     * @param HashMap data
+     */
+    public void updateTemp(HashMap data) {
+        String bean = Convert.DecimalPoint((Double)data.get("b"));
+        String stove = Convert.DecimalPoint((Double) data.get("s"));
+        String environment = Convert.DecimalPoint((Double) data.get("e"));
+
+        setBeansTemp(bean);
+        setStoveTemp(stove);
+        setEnvironmentTemp(environment);
     }
 
     private void addTemp() {
@@ -194,19 +241,5 @@ public class MainViewModel extends ViewModel {
         }
 
         this.setNowSetTemp(temp);
-    }
-
-    /**
-     * update information of temp column
-     * @param HashMap data
-     */
-    public void updateTemp(HashMap data) {
-        String bean = Convert.DecimalPoint((Double)data.get("b"));
-        String stove = Convert.DecimalPoint((Double) data.get("s"));
-        String environment = Convert.DecimalPoint((Double) data.get("e"));
-
-        setBeansTemp(bean);
-        setStoveTemp(stove);
-        setEnvironmentTemp(environment);
     }
 }
